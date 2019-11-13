6ffc675565e69a102f5d80c413daacb2c3b640d7
simonheglasik <simonheglasik@gmail.com>
2019-11-13 19:29:06 +0100

    Work in progress....

---
+++ Open-Lab-04.13/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_04._13
+{
+    class Class1
+    {
+        public string GetLongestCommonSequence(string str1,string str2)
+        {
+            string a = "";
+            return a;
+        }
+    }
+}
---
+++ Open-Lab-04.13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_04._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clas = new Class1();
+            Console.WriteLine(clas.GetLongestCommonSequence("mojstryko", "cstrike"));
+        }
+    }
+}
